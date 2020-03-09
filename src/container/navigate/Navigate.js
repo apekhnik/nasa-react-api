957dabe68be787a29e399cb4d70a37a4c78cb7c2
apekhnik <iam3ji0@gmail.com>
2020-03-09 18:16:33 +0300

    upgrade navigate

--- src/container/navigate/Navigate.js
+++ src/container/navigate/Navigate.js
@@ -9,15 +9,27 @@ const Navigate =()=>{
     const classname = classnames('navigate', vision)
     return(
         <div className={classname}>
-            <NavLink
-            to="/APOD"
-            className="components_list__item"
-           >TEST</NavLink>
-
-            <NavLink
-            to="/media-search"
-            className="components_list__item"
-           >new</NavLink>
+            <div className='navigate-item'>
+                    <NavLink
+                            to="/"
+                            className=""
+                            >HOME
+                    </NavLink>
+            </div>
+            <div className='navigate-item'>
+                    <NavLink
+                            to="/APOD"
+                            className=""
+                            >Astronimic Picture of Day
+                    </NavLink>
+            </div>
+            <div className='navigate-item'>
+                    <NavLink
+                            to="/media-search"
+                            className=""
+                            >NASA media search
+                    </NavLink>
+            </div>
            <Button text={navigate ? 'C' : 'O'} onClick={()=>setNavigate(!navigate)} className='nav-btn'/>
         </div>
     )
