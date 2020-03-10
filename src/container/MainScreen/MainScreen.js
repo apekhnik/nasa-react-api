5e1f3f757f0500d2163fafcb2a704d5c8a912bfd
apekhnik <iam3ji0@gmail.com>
2020-03-10 16:51:14 +0300

    animation welcome item

--- src/container/MainScreen/MainScreen.js
+++ src/container/MainScreen/MainScreen.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import APODpage from '../APODpage'
 import NASAIMAGE from '../NASAIMAGE'
+import WelcomePage from '../WelcomePage/WelcomePage'
 const Mainscreen =()=>{
         return(
             
                 <Switch>
                     <Route path="/APOD" component={APODpage}exact/>
                     <Route path="/media-search" component={NASAIMAGE}exact/>
+                    <Route path="/" component={WelcomePage}exact/>
                 </Switch>  
             
         )
---
+++ src/container/WelcomePage/WelcomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './WelcomePage.css'
+const WelcomePage =()=>{
+    return(
+        <div className="api-item__container">
+            <div className='api-item'>
+                <img src='https://www.1zoom.ru/big2/305/251601-Sepik.jpg' alt=''/>
+                <h3 className='first'>Astronomic</h3>
+                <h3 className='second'>Picture</h3>
+                <h3 className='third'>of the</h3>
+                <h3 className='fourth'>Day</h3>
+            </div>
+            <div className='api-item'>
+                <img src='https://www.zastavki.com/pictures/1920x1200/2011/Space_Bright_Star_030575_.jpg' alt=''/>
+            </div>
+            
+        </div>
+    )
+}
+export default WelcomePage
