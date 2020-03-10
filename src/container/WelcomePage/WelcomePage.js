96848b8f7713095ca22d1042bb29e121f8024964
apekhnik <iam3ji0@gmail.com>
2020-03-10 20:48:55 +0300

    modernize welcome page

--- src/container/WelcomePage/WelcomePage.js
+++ src/container/WelcomePage/WelcomePage.js
@@ -12,6 +12,9 @@ const WelcomePage =()=>{
             </div>
             <div className='api-item'>
                 <img src='https://www.zastavki.com/pictures/1920x1200/2011/Space_Bright_Star_030575_.jpg' alt=''/>
+                <h3 className='first2'>NASA</h3>
+                <h3 className='second2'>media</h3>
+                <h3 className='third2'>search</h3>
             </div>
             
         </div>
