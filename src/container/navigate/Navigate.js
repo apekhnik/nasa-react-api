9d027b2dde1164fedb301c6100339d9a5a78025a
apekhnik <iam3ji@gmail.com>
2020-06-11 17:24:33 +0300

    g

--- src/container/navigate/Navigate.js
+++ src/container/navigate/Navigate.js
@@ -1,37 +1,32 @@
-import React, {useState} from 'react'
-import './Navigate.css'
-import {NavLink} from 'react-router-dom'
-import classnames from 'classnames'
-import Button from '../../component/Button/Button'
-const Navigate =()=>{
-    const [navigate, setNavigate] = useState(false)
-    const vision = navigate ? 'nav-show':'nav-hide'
-    const classname = classnames('navigate', vision)
-    return(
-        <div className={classname}>
-            
-                    <NavLink
-                            to="/"
-                            className="navigate-item home"
-                            >HOME
-                    </NavLink>
-            
-            
-                    <NavLink
-                            to="/APOD"
-                            className="navigate-item"
-                            >Astronimic Picture of Day
-                    </NavLink>
-            
-            
-                    <NavLink
-                            to="/media-search"
-                            className="navigate-item"
-                            >NASA media search
-                    </NavLink>
-            
-           <Button text={navigate ? 'C' : 'O'} onClick={()=>setNavigate(!navigate)} className='nav-btn'/>
-        </div>
-    )
-}
-export default Navigate
+import React, { useState } from "react";
+import "./Navigate.css";
+import { NavLink } from "react-router-dom";
+import classnames from "classnames";
+import Button from "../../component/Button/Button";
+const Navigate = () => {
+  const [navigate, setNavigate] = useState(false);
+  const vision = navigate ? "nav-show" : "nav-hide";
+  const classname = classnames("navigate", vision);
+  return (
+    <div className={classname}>
+      <NavLink to={process.env.PUBLIC_URL + "/"} className="navigate-item home">
+        HOME
+      </NavLink>
+
+      <NavLink to="/APOD" className="navigate-item">
+        Astronimic Picture of Day
+      </NavLink>
+
+      <NavLink to="/media-search" className="navigate-item">
+        NASA media search
+      </NavLink>
+
+      <Button
+        text={navigate ? "C" : "O"}
+        onClick={() => setNavigate(!navigate)}
+        className="nav-btn"
+      />
+    </div>
+  );
+};
+export default Navigate;
