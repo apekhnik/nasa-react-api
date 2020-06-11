0c8a4dff198b8d4a103ce8ab91cd04011d320875
apekhnik <iam3ji@gmail.com>
2020-06-11 17:31:03 +0300

    some change

--- src/container/MainScreen/MainScreen.js
+++ src/container/MainScreen/MainScreen.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import APODpage from '../APODpage'
-import NASAIMAGE from '../NASAIMAGE'
-import WelcomePage from '../WelcomePage/WelcomePage'
-const Mainscreen =()=>{
-        return(
-            
-                <Switch>
-                    <Route path="/APOD" component={APODpage}exact/>
-                    <Route path="/media-search" component={NASAIMAGE}exact/>
-                    <Route path="/" component={WelcomePage}exact/>
-                </Switch>  
-            
-        )
-}
-export default Mainscreen
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import APODpage from "../APODpage";
+import NASAIMAGE from "../NASAIMAGE";
+import WelcomePage from "../WelcomePage/WelcomePage";
+const Mainscreen = () => {
+  return (
+    <Switch>
+      <Route
+        path={process.env.PUBLIC_URL + "/APOD"}
+        component={APODpage}
+        exact
+      />
+      <Route
+        path={process.env.PUBLIC_URL + "/media-search"}
+        component={NASAIMAGE}
+        exact
+      />
+      <Route
+        path={process.env.PUBLIC_URL + "/"}
+        component={WelcomePage}
+        exact
+      />
+    </Switch>
+  );
+};
+export default Mainscreen;
